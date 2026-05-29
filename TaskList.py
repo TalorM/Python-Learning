c5f79a0f1ae9b3d21c67fbf8edd8d3b220b6b9a6
Talor M <110429857+TalorM@users.noreply.github.com>
2026-05-29 15:42:24 -0600

    Added functionality to change the task file path

--- TaskList.py
+++ TaskList.py
@@ -1,36 +1,68 @@
 import time
+import json
+
+task_file_path = "C:\\source\\Python Learning\\Task-List.json"
+tasks = {}
 
 def ListTaskOptions():
   # TODO Clear console text - How do I make it a >>> prompt?
   # os.system('cls') is deprecated so..?
-  print("NOTE: The current default location is 'C:\source\Python Learning'")
+  print("NOTE: The current file path is " + task_file_path)
   print("Enter 0-5 to pick from the options below")
-  print("0 or 'Enter' - View Tasks at JSON file in default location.")
-  print("1 - Create new JSON Task file.")
-  print("2 - Change default location?")
-  option = input("Submit here: ")
+  print("0 or 'Enter' - View Tasks at current file path")
+  print("1 - Create new Task file")
+  print("2 - Change current path")
+  option = input("Enter option: ")
 
-  if option == 0:
+  if option == "0" or option == "" or option =="Enter":
     ListTasks()
-  elif option == 1:
-    CreateJSONFile()
-  elif option == 2:
-    print("Mmm idk how to do this yet but ill circle back if i can get it working nicely.")
-    time.sleep(3)
-    ListTaskOptions()
+  elif option == "1":
+    CreateTaskFile()
+  elif option == "2":
+    ChangeTaskFilePath()
   else:
     print("Not a valid option. Give it another try!")
     time.sleep(3)
     ListTaskOptions()
-  
+
   return
 
 def ListTasks():
+  # Pull tasks
+  # Display tasks
+  # Display options to edit, add, or remove
+  print("Grabbing Tasks from " + task_file_path)
+  # try:
+  #   with open(task_file_path)
+  # except
+  # tasks = json.load()
   return
 
-def CreateJSONFile():
+def ChangeTaskFilePath():
+  global task_file_path 
+  print("Your current path is " + task_file_path)
+  new_task_file_path = input("Enter new file path: (or press 'q' to quit.) ")
+  if new_task_file_path == "q" or new_task_file_path == "quit":
+    print("Returning to Main Menu...")
+    time.sleep(3)
+    ListTaskOptions()
+    return
+  else:
+    print("You entered " + new_task_file_path)
+    confirmation = input("Confirm? (y/n): ")
+    if confirmation == "y" or confirmation == "":
+      # TODO find how to make this permanent by editing and saving current file.
+      print("Confirmed! Returning to Main Menu...")
+      task_file_path = new_task_file_path
+      time.sleep(3)
+      ListTaskOptions()
+    else:
+      print("Canceled. Returning to previous input")
+      time.sleep(3)
+      ChangeTaskFilePath()
   return
 
-
+def CreateTaskFile():
+  return
 
 ListTaskOptions()
